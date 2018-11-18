8dd347228b92b3ac64758b982dd629369547e3b1
Przemek <dudek.mail@mail.com>
2018-11-18 12:17:27 +0100

    in progress...

--- src/main/java/pl/dudekjunior/familyapp/models/repositories/ChildRepository.java
+++ src/main/java/pl/dudekjunior/familyapp/models/repositories/ChildRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface ChildRepository extends CrudRepository<ChildEntity, Integer> {
-    public Optional<List<ChildEntity>> findByFatherId(int fatherId);
+    Optional<List<ChildEntity>> findByFatherId(int fatherId);
 }
